e2b978e5645f20113733bc0e31eb290384dc863c
sewon <shin6403@gmail.com>
2020-12-25 23:16:53 +0900

    리액트 네이티브 연습

--- App.js
+++ App.js
@@ -1,13 +1,41 @@
+import * as React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home 안녕</Text>
+      <Button title="Go to Details" onPress={() => navigation.navigate('Details')} />
+    </View>
+  );
+};
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>디테일 스크린입니다.</Text>
+      <Button title="Go to Details...again" onPress={() => navigation.push('Details')} />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go Back" onPress={() => navigation.goBack()} />
+      <Button title="Go Back to first screen in stack" onPress={() => navigation.popToTop()} />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'OverView' }} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
